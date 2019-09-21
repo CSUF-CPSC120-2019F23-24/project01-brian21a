d68b8181962609fa165ac76f98b49bf0b837e1db
Brian Smetana <54484285+brian21a@users.noreply.github.com>
2019-09-21 15:55:46 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,38 @@
 //Brian Smetana:
 //This is a program for setting up a Business Trip Tracker
 	#include <iostream>
-//to use setw()	
-	#include <iomanip> 
+//to use setw()
+	#include <iomanip>
+  #include <string>
 	int main ()
 	{
-	std::cout << "Welcome to the Business Trip Tracker!\n";
+	std::cout << "Welcome to the Business Trip Tracker!" << std::endl << std::endl;
 // we will use the following variables: Location, Days, HotelCost, MealCost, TotalCost
 			int Days;
 			double HotelCost, MealCost, TotalCost;
+      std::string Location; //to use string
+    std::cout << "What is the business trip location? ";
+    std::cin >> Location;
+//asks user for Location
+    std::cout << "How Many days will the trip take? ";
+    std::cin >> Days;
 //asks user for location
-		std::cout << "What is the business trip location?";
-		std::cin >> Location;
-		std::string >> Location;
-		
-		std::cout << "What is the total hotel expense?';
+
+		std::cout << "What is the total hotel expense? ";
+    std::cout << '$';
 		std::cin >> HotelCost;
 //HotelCost obtained
-		
-		std::cout << " What is the total meal expense?";
+
+		std::cout << " What is the total meal expense? ";
+    std::cout << '$';
 		std::cin >> MealCost;
 //MealCost obtained
-		std::cin >> TotalCost = MealCost + HotelCost;
+TotalCost = MealCost + HotelCost;
 //calculates total cost
-		std::cout << "Location" << std::setw(9) << "Days" << std::setw(10)
-  << "Hotel" << std::setw(9) << "Meal" << std::setw(10) << "Total\n";
+		std::cout << "Location: " << std::setw(10)  << "Days: " << std::setw(9) << "Hotel expense: " << std::setw(8) << "Meal expense: " << std::setw(7) << "Total expenses: " << std::setw(6) << std::endl;
+    std::cout <<  Location << std::setw(10) << Days << std::setw(9) << HotelCost << std::setw(8) << MealCost << std::setw(3) << TotalCost << std::setw(1) << std::endl;
  //formats it correctly
-	}
-	
-		return 0;
+
+	return 0;
+}
+
